58a31d227190f1ab72acf15a53c892684bac5393
Astar <bahouidi99@gmail.com>
2025-09-18 15:47:31 +0100

    Shell: initialize wish project

---
+++ processes-shell/script.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+# Created on: Thu Sep 18 15:43:41 +01 2025
---
+++ processes-shell/wish.c
@@ -0,0 +1,11 @@
+/* ostep-projects/processes-shell/wish.c */
+// Crated on: Thu Sep 18 15:14:14 +01 2025
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[argc + 1]) {
+
+  /* Code */
+
+  return EXIT_SUCCESS;
+}
